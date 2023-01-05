426a82d1f53492c909a616e2514ee3fdf7ae4bf1
Pawel Czyz <pawelczyz55@gmail.com>
2023-01-05 15:15:15 +0100

    Added flaskforms

--- website/models.py
+++ website/models.py
@@ -4,7 +4,9 @@ from sqlalchemy.sql import func
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
+    #data = db.Column(db.String(10000))
+    filename = db.Column(db.String(50))
+    data = db.Column(db.LargeBinary)
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
